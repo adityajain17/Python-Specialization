94fc189ea877c195dc8d3461a219969279a69f2f
Aditya Jain <adtjn9@gmail.com>
2019-05-24 23:19:37 +0530

    Add files via upload

---
+++ C3_Week5_1.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+
+import json
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location : ')
+print('Retrieving ',url)
+html = urlopen(url, context=ctx).read()
+print('Retrieved',len(html.decode()),'characters')
+
+tree= ET.fromstring(html.decode())
+c=0
+s=0
+
+lst = tree.findall('comments/comment/count')
+print('Count: ',len(lst))
+s=0
+for c in lst:
+    s=s+int(c.text)
+print('Sum: ',s)
--- test.py
+++ test.py
@@ -1,14 +1,23 @@
-# Search for link values within URL input
 import urllib.request, urllib.parse, urllib.error
-import re
+from bs4 import BeautifulSoup
 import ssl
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-url = input('Enter - ')
-html = urllib.request.urlopen(url).read()
-links = re.findall(b'href="(http[s]?://.*?)"', html)
-for link in links:
-    print(link.decode())
-# Code: http://www.py4e.com/code3/urlregex.py
+
+url = input('Enter URL : ')
+count=int(input('Enter count : '))
+pos=int(input('Enter position : '))
+
+for i in range(count+1):
+    print('Retrieving: ',url)
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    c=0
+    for tag in tags:
+        c=c+1
+        if c==pos:
+          url=tag.get('href', None)
