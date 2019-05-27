f676b656267023ae5c8472674baded4ccb065475
Aditya Jain <adtjn9@gmail.com>
2019-05-27 23:53:39 +0530

    Add files via upload

---
+++ SQL-test.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn=sqlite3.connect('Python_C3.db')
+cur=conn.cursor()
+print(cur)
+
+
+cur.execute('INSERT INTO Tracks (title, plays) VALUES (?, ?)',
+('Thunderstruck', 20))
+print(cur.execute('INSERT INTO Tracks (title, plays) VALUES (?, ?)',
+('My Way', 15)))
+conn.commit()
+cur.execute('SELECT * FROM Tracks')
+for row in cur:
+    print(row)
+cur.close();
+conn.close();
